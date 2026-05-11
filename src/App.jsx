3ccf2080618876159c8f0815c8b877c240040148
yepsonsteeve <yepsonsteeve@gmail.com>
2026-05-11 23:22:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+const { useState } = React;
 import { AreaChart, Area, PieChart, Pie, Cell, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 const INITIAL_EXPENSES = [
