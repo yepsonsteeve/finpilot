30146678a6496943095333e5f74c5b4b2883ad96
yepsonsteeve <yepsonsteeve@gmail.com>
2026-05-14 00:06:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import * as React from ‘react’;
-const { useState, useEffect } = React;
+import React, { useState, useEffect } from 'react';
 import { AreaChart, Area, LineChart, Line, BarChart, Bar, PieChart, Pie, Cell, XAxis, YAxis, Tooltip, ResponsiveContainer } from “recharts”;
 
 // ─── Storage ──────────────────────────────────────────────────────────────────
